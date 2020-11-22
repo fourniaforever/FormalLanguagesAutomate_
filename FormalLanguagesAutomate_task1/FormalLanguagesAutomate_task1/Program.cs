577a7a27132fff8c89c1d0c72165f7bad2a4cd4b
seikosan <ionova_dasha123@mail.ru>
2020-11-22 18:27:47 +0400

    First task with int

---
+++ FormalLanguagesAutomate_task1/FormalLanguagesAutomate_task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FormalLanguagesAutomate_task1
+{
+    class Program
+    {
+        static public Automate GetAutomate(string fileName)
+        {
+
+            Automate automate = new Automate();
+            using (StreamReader streamReader = new StreamReader(fileName))
+            {
+                string s = streamReader.ReadToEnd();
+                automate = JsonConvert.DeserializeObject<Automate>(s);
+            }
+            return automate;
+        }
+
+        static void Main(string[] args)
+        {
+                Automate automation = GetAutomate("INT.txt");
+
+                Console.WriteLine("Input text to recognise:");
+                string inf = Console.ReadLine();
+
+                Console.WriteLine("Set the offset:");
+
+                if (int.TryParse(Console.ReadLine(), out var offset) && offset >= 0)
+                {
+                    var result = automation.Move(inf, offset);
+
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect offset");
+                }
+
+                Console.ReadKey();
+        }
+    }
+}
